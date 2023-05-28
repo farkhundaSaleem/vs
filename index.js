842ffb51200c4e0631a605e4e248bd61734bdfaf
Farkhunda Saleem <96863402+farkhundaSaleem@users.noreply.github.com>
2023-05-28 13:19:57 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+function popup(message) {
+     alert(message);
+     }
+    
+
+
+      function checkAddress(fieldId) {
+         if (document.getElementById(fieldId).value === "") {
+         alert("Email address required.");
+         }
+         }
+        
+
+
+
+          function expandLoris() {
+             var expandedParagraph = "Thankyou fo clicking here you have been sibmitted !";
+             document.getElementById("slowLoris").innerHTML = expandedParagraph;
+             }
+            
